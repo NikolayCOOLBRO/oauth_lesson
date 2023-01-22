ce63b88f1c9030b7598dc0f5249fe32a188da294
nikolay.kovalenko <kolya03kovalenko@yandex.ru>
2023-01-22 16:32:55 +0700

    init

---
+++ Notes.Identity/Startup.cs
@@ -0,0 +1,44 @@
+﻿using IdentityServer4.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+
+namespace Notes.Identity
+{
+    public class Startup
+    {
+        public IConfiguration AppConfiguration { get; }
+
+        public Startup(IConfiguration configuration) =>
+            AppConfiguration = configuration;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = AppConfiguration.GetValue<string>("DbConnection");
+
+            services.AddIdentityServer()
+                .AddInMemoryApiResources(Configuration.ApiResources)
+                .AddInMemoryIdentityResources(Configuration.IdentityResources)
+                .AddInMemoryApiScopes(Configuration.ApiScopes)
+                .AddInMemoryClients(Configuration.Clients)
+                .AddDeveloperSigningCredential();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseIdentityServer();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
